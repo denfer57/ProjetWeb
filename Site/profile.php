1a2ce31a9fd413a78b656f1a11275ae01631ba05
denfer57 <jordan.fromeyer@gmail.com>
2017-01-02 22:36:53 +0100

    2ème recommandation
    Ajout de la 2ème recommandation

--- Site/profile.php
+++ Site/profile.php
@@ -28,7 +28,7 @@
 
 	if(count($idepisode)>0) $html.= '<div>
         <div class="col-lg-3">
-		<p style="font-weight: bold"> Vos épisodes vus sont :</p>';
+		<p style="font-weight: bold">Your episodes you have seen are :</p>';
 	else $html.= '<p style="font-weight: bold"> Vous n\'avez pas enregistrés d\'épisodes que vous avez vus.</p>';
 
 	//On recupère le nom des épisodes ainsi que leurs images.
@@ -103,7 +103,7 @@
 		$idgenre[$i] = $rowgen[0];
 	}
 
-	//1 ère recommandation
+	//1 ère recommandation : des séries choisies au hasard parmi les séries dont l’une des thématiques correspond à la thématique la plus fréquente de l’utilisateur
 	//On regarde 5 séries similaires au hasard du même genre pour l'utilisateur
 	$querygenre = "SELECT series_id
 	FROM seriesgenres
@@ -113,7 +113,8 @@
 	$statement = $connexion->prepare($querygenre);
 
 	//On associe un genre aléatoire parmi les genres des épisodes de l'utilisateur
-	$statement->bindValue(":idgenre", $idgenre[rand(0, count($idepisode)-1)], PDO::PARAM_STR);
+	$randgenre = rand(0, count($idepisode)-1);
+	$statement->bindValue(":idgenre", $idgenre[$randgenre] , PDO::PARAM_STR);
 	$statement->execute();
 	$rowgen = $statement->fetchAll(PDO::FETCH_COLUMN, 0);
 	for($i=0;$i<5;$i++){
@@ -134,8 +135,19 @@
 		$imgserie[$i] = $rowseasonalea[1];
 	}
 
+	//On recupere le nom du genre
+	$querynamegenre = "SELECT name
+	FROM genres";
+	$statement = $connexion->prepare($querynamegenre);
+	for($i=0;$i<count($idepisode);$i++){
+		$statement->bindValue(":idgenre", $idgenre[$randgenre], PDO::PARAM_STR);
+		$statement->execute();
+		$rowgen = $statement->fetch();
+		$namegenre = $rowgen[0];
+	}
+
 	$html .= '</div><div class="col-lg-3">
-	<p style="font-weight: bold">Vos recommandations :</p>';
+	<p style="font-weight: bold">Your recommandations for the genre '.$namegenre.', five similaries series :</p>';
 
 	for($i=0;$i<5;$i++){
 		$html .= '<p>Série : '.$nameserie[$i].'</p>';
@@ -145,13 +157,56 @@
 			<img src="http://localhost/Projetweb/Site/images/photo_manquante.jpg" alt="Pas d\'image"/></a>';
 	}
 
-	//2ème recommandation 
+	//2ème recommandation : des séries choisies au hasard parmi les séries du même réalisateur que la série actuellement visualisée sur le site.
+	//On recupere l'id du réalisateur
+	$querynamegenre = "SELECT creator_id
+	FROM seriescreators
+	WHERE series_id = :idserie";
+	$statement = $connexion->prepare($querynamegenre);
+	for($i=0;$i<count($idepisode);$i++){
+		$statement->bindValue(":idserie", $idserie[$randgenre], PDO::PARAM_STR);
+		$statement->execute();
+		$rowgen = $statement->fetch();
+		$idcreator = $rowgen[0];
+	}
 
-	
-	/*
-	(1) des séries choisies au hasard parmi les séries dont l’une des thématiques correspond à la thématique la plus fréquente de l’utilisateur ;
-	(2) des séries choisies au hasard parmi les séries du même réalisateur que la série actuellement visualisée sur le site.
-	*/
+	//On recupère les infos des réalisateurs
+	$querygenre = "SELECT series_id
+	FROM seriescreators
+	WHERE creator_id = :idcreator
+	ORDER BY RAND()
+	LIMIT 1";
+	$statement = $connexion->prepare($querygenre);
+
+	//On associe des séries aléatoires avec un realisateur similaire a la serie visionnée
+	$statement->bindValue(":idcreator", $idcreator , PDO::PARAM_STR);
+	$statement->execute();
+	$rowgen2 = $statement->fetchAll(PDO::FETCH_COLUMN, 0);
+	$seriesimi2 = $rowgen2[0];
+
+	//On recupere les infos des séries aléatoires
+	$queryseriealea = "SELECT name, poster_path
+	FROM series
+	WHERE id = :seriesimi2";
+	$statement = $connexion->prepare($queryseriealea);
+
+	for($i=0;$i<5;$i++){
+		$statement->bindValue(":seriesimi2", $seriesimi2, PDO::PARAM_STR);
+		$statement->execute();
+		$rowseasonalea = $statement->fetch();
+		$nameserie2 = $rowseasonalea[0];
+		$imgserie2 = $rowseasonalea[1];
+	}
+
+	$html .= '</div><div class="col-lg-3">
+	<p style="font-weight: bold">Serie with the same creator :</p>';
+
+	$html .= '<p>Série : '.$nameserie2.'</p>';
+  	if ($imgserie!=NULL) $html .= '<a href="http://localhost/Projetweb/Site/detail_serie.php?idserie='.$seriesimi2.'">
+  		<img src="https://image.tmdb.org/t/p/w185'.$imgserie2.'" alt="'.$nameserie2.'" id="imgseriesaison"/></a>';
+	else $html .= '<a href="http://localhost/Projetweb/Site/detail_serie.php?idserie='.$seriesimi2.'">
+		<img src="http://localhost/Projetweb/Site/images/photo_manquante.jpg" alt="Pas d\'image"/></a>';
+
 	/*Changer de mot de passe ?
 	$html.= '<form action="http://localhost/Projetweb/Site/changermdp.php" method="post">
 		<div><label for="mdp">Nouveau mot de passe : </label><input type="password" name="mdp" id="mdp" required=""/></div>
